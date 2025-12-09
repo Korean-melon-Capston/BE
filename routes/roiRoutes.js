dba4af4dd8d4e01eacf6e89a0f5f6d728ad22815
JunsangLee <juoc03@gmail.com>
2025-12-10 02:50:35 +0900

    dd

--- routes/roiRoutes.js
+++ routes/roiRoutes.js
@@ -1,7 +1,6 @@
 // routes/roiRoutes.js
 import { Router } from "express";
 import { saveROI, getROIStatus } from "../controllers/roiController.js";
-import auth from "../middlewares/auth.js";
 
 const router = Router();
 
@@ -9,14 +8,14 @@ const router = Router();
  * ROI 저장/수정
  * POST /api/roi
  */
-router.post("/", auth, saveROI);
+router.post("/", saveROI);
 // 필요하면 테스트용으로 auth 뺄 수도 있음: router.post("/", saveROI);
 
 /**
  * ROI 조회
  * GET /api/roi
  */
-router.get("/", auth, getROIStatus);
+router.get("/", getROIStatus);
 // 테스트용: router.get("/", getROIStatus);
 
 export default router;
